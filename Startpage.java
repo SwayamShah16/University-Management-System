a40cb55b4dcb238a3302b4318a2f5787b2213347
SwayamShah16 <shahswayam222@gmail.com>
2024-10-11 18:12:29 +0530

    Update and rename Splash.java to Startpage.java

--- Startpage.java
+++ Startpage.java
@@ -3,9 +3,9 @@ package university.management.system;
 import javax.swing.*;
 import java.awt.*;
 
-public class Splash extends JFrame implements Runnable {
+public class Startpage extends JFrame implements Runnable {
     Thread t;
-    Splash(){
+    Startpage(){
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/first.png"));
         Image i2 = i1.getImage().getScaledInstance(1000,700,Image.SCALE_DEFAULT);
@@ -42,6 +42,6 @@ public class Splash extends JFrame implements Runnable {
     }
 
     public static void main(String[] args) {
-        new Splash();
+        new Startpage();
     }
 }
